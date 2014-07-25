761cc91321b91b90da391b26a2e6ccc3b3ff5dc5
Antoine Frénoy <afrenoy@gmx.com>
2014-07-25 14:04:52 +0000

    Update movies.py:   Do not regenerate existing png by default.   Add an option to regenerate them.   Pass directly output file name to treat_generation.   Only produce a movie with asked frames even if more png exist.

--- src/misc/movies.py
+++ src/misc/movies.py
@@ -7,7 +7,7 @@ import getopt
 import png
 import glob
 
-def treat_generation(gfile,name,num,xsize,ysize,minv=0.,maxv=1.,colors=(0,255,0)):
+def treat_generation(gfile,namepng,xsize,ysize,minv=0.,maxv=1.,colors=(0,255,0)):
     (rscale,gscale,bscale)=colors
     table=numpy.zeros((xsize,ysize))
     f = open(gfile,'r')
@@ -23,7 +23,6 @@ def treat_generation(gfile,name,num,xsize,ysize,minv=0.,maxv=1.,colors=(0,255,0)
     f.close()
     pix=[tuple(reduce(lambda x,y:x+y, [(int((j-minv)/(maxv-minv)*rscale),int((j-minv)/(maxv-minv)*gscale),int((j-minv)/(maxv-minv)*bscale)) for j in i])) for i in table]
 
-    namepng='_%s%04d.png'%(name,num)
     pngfile=open(namepng,'wb')
     wr=png.Writer(xsize,ysize)
     wr.write(pngfile,pix)
@@ -34,7 +33,7 @@ def printhelp():
     print name +': a script to produce a movie from an aevol simulation with dumps activated.'
     print 'You need to have ffmpeg installed and in your path.'
     print 'Usage:'
-    print '\t' + name + ' -i inputname [-s firstgen] [-e lastgen] [-p patchsize] [-m minvalue] [-M maxvalue] [-c color] [-k]'
+    print '\t' + name + ' -i inputname [-s firstgen] [-e lastgen] [-p patchsize] [-m minvalue] [-M maxvalue] [-c color] [-o] [-k] sim1 sim2 .. simN'
     print 'or'
     print '\t' + name + ' -h'
     print 'Example:'
@@ -43,7 +42,7 @@ def printhelp():
     exit(0)
 
 
-opts, args = getopt.getopt(sys.argv[1:], "i:s:e:p:m:M:c:kh", ["inputname","start","end","patchsize","minvalue","maxvalue","color","keep","help"])
+opts, args = getopt.getopt(sys.argv[1:], "i:s:e:p:m:M:c:kho", ["inputname","start","end","patchsize","minvalue","maxvalue","color","keep","help","overwrite"])
 
 start=None
 end=None
@@ -53,6 +52,7 @@ minvalue=0.
 maxvalue=1.
 colors=(0,255,0)
 keepinter=False
+overwrite=False
 
 for o, a in opts:
     if o=='-i' or o=='--inputname': # Name of the output to treat (basename of the matching dump files, for example 'fitness_metabolic'). 
@@ -79,30 +79,38 @@ for o, a in opts:
         else:
             print 'Error: unrecognized argument given to -c option'
             exit(-1)
-    elif o=='-k' or o=='--keep':
+    elif o=='-k' or o=='--keep': # do not erase png files
         keepinter=True
+    elif o=='-o' or o=='--overwrite': # re-generate png files if already existing
+        overwrite=True
     elif o=='-h' or o=='--help':
         printhelp()
+    else:
+        print 'Error: unrecognized option ' + o
+        exit(-1)
 
 if not inputname:
     print 'Error: you must specifiy option -i (--inputname)'
     exit(-1)
 
-if len(glob.glob("_"+inputname+"*.png"))>0:
-    print 'Warning: about to overwrite existing png files. 5 seconds left to abort ...'
-    import time
-    time.sleep(5)
+smallestgen=1000000
+biggestgen=None
+warned=False
 
 for arg in args: # If several folders are given as arguments treat all of them separatly
     first=True
     for filename in os.listdir(arg+'/stats/dump/'):
         if filename.startswith(inputname):
             ngen=int(filename.split('_').pop().split('.')[0])
+            if ngen>biggestgen:
+                biggestgen=ngen
+            if ngen<smallestgen:
+                smallestgen=ngen
             if first: # Use this first file to infer size of the grid
                 mx=-1
                 my=-1
                 first=False
-                temp=open(arg+'/stats/dump/'+inputname+'_%04d.out'%ngen)
+                temp=open(arg+'/stats/dump/'+inputname+'_%06d.out'%ngen)
                 for line in temp:
                     if len(line)<=3 or line.startswith('#'):
                         continue
@@ -116,8 +124,19 @@ for arg in args: # If several folders are given as arguments treat all of them s
                 print "Detected x size: %d, detected y size: %d"%(mx+1,my+1)
             if (start and ngen<start) or (end and ngen>end): # do not treat generations that are not in given interval if any
                 continue
-            treat_generation(arg+'/stats/dump/'+inputname+'_%04d.out'%ngen,inputname,ngen,mx+1,my+1,minvalue,maxvalue,colors)
-    os.system("ffmpeg -pattern_type glob -i '_" + inputname + "*.png' -r 6  -vcodec png -s "+str((mx+1)*patchsize)+"x"+str((my+1)*patchsize)+" -sws_flags neighbor -sws_dither none " + arg + '/' + inputname + ".mov")
+            namepng='_%s%06d.png'%(inputname,ngen)
+            if not os.path.exists(namepng) or overwrite:
+                treat_generation(arg+'/stats/dump/'+inputname+'_%06d.out'%ngen,namepng,mx+1,my+1,minvalue,maxvalue,colors)
+            elif not warned:
+                print ''
+                print "Warning: already existing png files found, we will use them instead of re-generating. If you want to re-generate them (to use different options), you first need to delete existing png files or to use option --overwrite"
+                print ''
+                warned=True
+    if not start:
+        start=smallestgen
+    if not end:
+        end=biggestgen
+    os.system("ffmpeg -start_number " + str(start) + " -i _" + inputname + "%06d.png -vframes " + str(end-start) + " -r 6  -vcodec png -s "+str((mx+1)*patchsize)+"x"+str((my+1)*patchsize)+" -sws_flags neighbor -sws_dither none " + arg + '/' + inputname + ".mov")
     if not keepinter:
         os.system('rm _' + inputname + '*.png')
 
