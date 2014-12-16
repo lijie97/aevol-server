01ef3ff57a1d16d2b045c404e230e63c017a016f
Antoine Frénoy <afrenoy@gmx.com>
2014-12-16 20:10:15 +0100

    When environment is spatially structured, dump int_probes in the order of the grid.
    Previously it was dumped according to the order of the list of
    individuals.
    When the environment is not spatially structured, we keep the old
    behaviour.

--- src/libaevol/ae_dump.cpp
+++ src/libaevol/ae_dump.cpp
@@ -216,35 +216,55 @@ void ae_dump::write_individual_probes( void )
   current_file = fopen( filename_buffer, "w" );
   
   fprintf(current_file, "Id\tInt_Probe_1\tInt_Probe_2\tInt_Probe_3\tInt_Probe_4\tInt_Probe_5\tDouble_Probe_1\tDouble_Probe_2\tDouble_Probe_3\tDouble_Probe_4\tDouble_Probe_5\n");
-  
-  ae_list_node<ae_individual*>* indiv_node  = _exp_m->get_indivs()->get_first();
-  ae_individual*  indiv       = NULL;
-  
-  int32_t* int_probes;
-  double* double_probes;
-  int32_t index;
 
-  while ( indiv_node != NULL )
+
+  if ( _exp_m->is_spatially_structured() )
   {
-    indiv = indiv_node->get_obj();
-    index = indiv->get_id();
-    int_probes    = indiv->get_int_probes();
-    double_probes = indiv->get_double_probes();
-    
-    fprintf(current_file,"%" PRId32, index);
-    for(int16_t i = 0; i < 5; i++)
+    for( int16_t x = 0 ; x < _exp_m->get_grid_width() ; x++ )
     {
-      fprintf(current_file,"\t%" PRId32,int_probes[i]);
+      for( int16_t y = 0 ; y < _exp_m->get_grid_height() ; y++ )
+      {
+        fprintf( current_file, "%" PRId32, _exp_m->get_spatial_structure()->get_indiv_at(x,y)->get_id() );
+        int32_t* int_probes = _exp_m->get_spatial_structure()->get_indiv_at(x,y)->get_int_probes();
+        double* double_probes = _exp_m->get_spatial_structure()->get_indiv_at(x,y)->get_double_probes();
+        for( int16_t i=0; i<5; i++) fprintf( current_file, "\t%" PRId32, int_probes[i] );
+        for( int16_t i=0; i<5; i++) fprintf( current_file, "\t%f", double_probes[i] );
+        fprintf( current_file, "\n" );
+      }
     }
-    for(int16_t i = 0; i < 5; i++)
-    {
-      fprintf(current_file,"\t%f",double_probes[i]);
-    }
-    fprintf(current_file,"\n");
-    
-    indiv_node = indiv_node->get_next();
+    fflush( current_file );
+    fclose( current_file );
   }
-  
+  else
+  {
+    ae_list_node<ae_individual*>* indiv_node  = _exp_m->get_indivs()->get_first();
+    ae_individual*  indiv       = NULL;
+
+    int32_t* int_probes;
+    double* double_probes;
+    int32_t index;
+
+    while ( indiv_node != NULL )
+    {
+      indiv = indiv_node->get_obj();
+      index = indiv->get_id();
+      int_probes    = indiv->get_int_probes();
+      double_probes = indiv->get_double_probes();
+
+      fprintf(current_file,"%" PRId32, index);
+      for(int16_t i = 0; i < 5; i++)
+      {
+        fprintf(current_file,"\t%" PRId32,int_probes[i]);
+      }
+      for(int16_t i = 0; i < 5; i++)
+      {
+        fprintf(current_file,"\t%f",double_probes[i]);
+      }
+      fprintf(current_file,"\n");
+
+      indiv_node = indiv_node->get_next();
+    }
+  }
   fflush( current_file );
   fclose( current_file );
 }
