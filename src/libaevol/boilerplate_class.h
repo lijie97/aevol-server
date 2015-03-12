7b99f9b50d0d3d454d447a1a89589495b23d896a
David Parsons <david.parsons@inria.fr>
2015-03-12 11:00:22 +0100

    Rename ae_template => BoilerplateClass

---
+++ src/libaevol/boilerplate_class.h
@@ -0,0 +1,116 @@
+// ****************************************************************************
+//
+//          Aevol - An in silico experimental evolution platform
+//
+// ****************************************************************************
+// 
+// Copyright: See the AUTHORS file provided with the package or <www.aevol.fr>
+// Web: http://www.aevol.fr/
+// E-mail: See <http://www.aevol.fr/contact/>
+// Original Authors : Guillaume Beslon, Carole Knibbe, David Parsons
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+//*****************************************************************************
+
+
+#ifndef __AE_BOILERPLATE_CLASS_H__
+#define __AE_BOILERPLATE_CLASS_H__
+
+
+// ============================================================================
+//                                   Includes
+// ============================================================================
+#include <cinttypes>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+
+
+namespace aevol {
+
+// ============================================================================
+//                          Class declarations
+// ============================================================================
+
+
+
+
+
+
+class BoilerplateClass
+{
+ public :
+  // ==========================================================================
+  //                               Constructors
+  // ==========================================================================
+  BoilerplateClass(void) = default; //< Default ctor
+  BoilerplateClass(const BoilerplateClass&) = delete; //< Copy ctor
+  BoilerplateClass(BoilerplateClass&&) = delete; //< Move ctor
+
+  // ==========================================================================
+  //                                Destructor
+  // ==========================================================================
+  virtual ~BoilerplateClass(void) = default; //< Destructor
+
+  // ==========================================================================
+  //                                 Getters
+  // ==========================================================================
+
+  // ==========================================================================
+  //                                 Setters
+  // ==========================================================================
+
+  // ==========================================================================
+  //                                Operators
+  // ==========================================================================
+
+  // ==========================================================================
+  //                              Public Methods
+  // ==========================================================================
+
+
+
+
+
+ protected :
+  // ==========================================================================
+  //                            Protected Methods
+  // ==========================================================================
+
+  // ==========================================================================
+  //                               Attributes
+  // ==========================================================================
+};
+
+
+// ============================================================================
+//                           Getters' definitions
+// ============================================================================
+
+// ============================================================================
+//                           Setters' definitions
+// ============================================================================
+
+// ============================================================================
+//                          Operators' definitions
+// ============================================================================
+
+// ============================================================================
+//                       Inline functions' definition
+// ============================================================================
+
+} // namespace aevol
+
+#endif // __AE_BOILERPLATE_CLASS_H__
