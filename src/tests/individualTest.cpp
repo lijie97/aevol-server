cb9236c4e7e038ded2799e1d8d28a6b698644ae2
David Parsons <david.parsons@inria.fr>
2015-04-23 18:21:53 +0200

    Rehab individual test

--- src/tests/individualTest.cpp
+++ src/tests/individualTest.cpp
@@ -28,30 +28,28 @@
 
 
 // =================================================================
-//                              Libraries
+//                              Includes
 // =================================================================
 #include <inttypes.h>
+#include <cstring>
+
+#include <list>
+#include <memory>
+
 #include <gtest/gtest.h>
-#include <string.h>
 
-
-
-// =================================================================
-//                            Project Files
-// =================================================================
 #include "ae_individual.h"
 #include "macros.h"
 #include "genetic_unit.h"
 #include "ae_rna.h"
 #include "ae_protein.h"
 #include "ae_params_mut.h"
+#include "../libaevol/macros.h"
 
 
-
-// ===========================================================================
-//                             Declare Used Namespaces
-// ===========================================================================
 using namespace aevol;
+
+using std::list;
 
 
 
@@ -119,7 +117,7 @@ void individualTest::SetUp(void)
 
   // Build indiv1
   ae_params_mut params_mut;
-  indiv1 = new ae_individual(NULL, NULL, NULL, &params_mut, 1.0, 10, 1000, false, 1, "anon-strain-1", 0);
+  indiv1 = new ae_individual(nullptr, nullptr, nullptr, std::make_shared<ae_params_mut>(params_mut), 1.0, 10, 1000, false, 1, "anon-strain-1", 0);
   indiv1->add_GU(genome, strlen(genome));
   genome = NULL;
 
@@ -130,8 +128,9 @@ void individualTest::SetUp(void)
 
 
   // Build indiv2
-  genome = indiv1->get_genetic_unit(0)->get_dna()->get_subsequence(0,0,LAGGING);
-  indiv2 = new ae_individual(NULL, NULL, NULL, &params_mut, 1.0, 10, 1000, false, 1, "anon-strain-2", 0);
+  // Reverse the whole genome
+  genome = indiv1->get_genetic_unit(0).get_dna()->get_subsequence(0,0,LAGGING);
+  indiv2 = new ae_individual(nullptr, nullptr, nullptr, std::make_shared<ae_params_mut>(params_mut), 1.0, 10, 1000, false, 1, "anon-strain-2", 0);
   indiv2->add_GU(genome, strlen(genome));
   genome = NULL;
 
@@ -145,7 +144,7 @@ void individualTest::SetUp(void)
   // Build indiv3
   genome = new char[1024];
   sprintf( genome, "%s%s%s%s%s%s%s", as[0], gene, as[1], term, as[2], prom[1], as[3]);
-  indiv3 = new ae_individual(NULL, NULL, NULL, &params_mut, 1.0, 10, 1000, false, 1, "anon-strain-3", 0);
+  indiv3 = new ae_individual(nullptr, nullptr, nullptr, std::make_shared<ae_params_mut>(params_mut), 1.0, 10, 1000, false, 1, "anon-strain-3", 0);
   indiv3->add_GU(genome, strlen(genome));
   genome = NULL;
 
@@ -157,8 +156,8 @@ void individualTest::SetUp(void)
 
 
   // Build indiv4
-  genome = indiv3->get_genetic_unit(0)->get_dna()->get_subsequence(0,0,LAGGING);
-  indiv4 = new ae_individual(NULL, NULL, NULL, &params_mut, 1.0, 10, 1000, false, 1, "anon-strain-4", 0);
+  genome = indiv3->get_genetic_unit(0).get_dna()->get_subsequence(0,0,LAGGING);
+  indiv4 = new ae_individual(nullptr, nullptr, nullptr, std::make_shared<ae_params_mut>(params_mut), 1.0, 10, 1000, false, 1, "anon-strain-4", 0);
   indiv4->add_GU(genome, strlen(genome));
   genome = NULL;
 
@@ -170,8 +169,8 @@ void individualTest::SetUp(void)
   // ***************************************************************************
   // The following commented code allows to print stuff about rnas and proteins
 
-  // printf("%"PRId32" rnas and %"PRId32" prots\n", indiv4->get_rna_list()->get_nb_elts(), indiv4->get_protein_list()->get_nb_elts());
-  // ae_list_node<ae_rna*>* rna_node = indiv4->get_rna_list()->get_first();
+  // printf("%"PRId32" rnas and %"PRId32" prots\n", indiv4->get_rna_list()->size(), indiv4->get_protein_list()->size());
+  // list_node<ae_rna*>* rna_node = indiv4->get_rna_list()->get_first();
   // while (rna_node != NULL)
   // {
   //   printf("%s rna at pos %"PRId32" (%f, %"PRId32")\n",
@@ -183,7 +182,7 @@ void individualTest::SetUp(void)
   //   rna_node = rna_node->get_next();
   // }
 
-  // ae_list_node<ae_protein*>* protein_node = indiv4->get_protein_list()->get_first();
+  // list_node<ae_protein*>* protein_node = indiv4->get_protein_list()->get_first();
   // while (protein_node != NULL)
   // {
   //   printf("%s protein at pos %"PRId32" (length: %"PRId32", concentr: %f, nb_rnas: %"PRId32")\n",
@@ -191,7 +190,7 @@ void individualTest::SetUp(void)
   //           protein_node->get_obj()->get_shine_dal_pos(),
   //           protein_node->get_obj()->get_length(),
   //           protein_node->get_obj()->get_concentration(),
-  //           protein_node->get_obj()->get_rna_list()->get_nb_elts());
+  //           protein_node->get_obj()->get_rna_list()->size());
 
   //   protein_node = protein_node->get_next();
   // }
@@ -208,33 +207,37 @@ void individualTest::TearDown(void)
 
 TEST_F(individualTest, TestIndiv1)
 {
+  // Check that we have the right number of promoters, terminators etc
+  // and at the right positions
+  // "right" means those values we have computed by hand
+
   // Check genome size
   EXPECT_EQ(109, indiv1->get_amount_of_dna());
   EXPECT_EQ(109, indiv1->get_genetic_unit_seq_length(0) );
 
   // Check RNA list
-  ae_list<ae_rna*>* rna_list = indiv1->get_rna_list();
-  EXPECT_EQ(2, rna_list->get_nb_elts() );
-  ae_rna* rna = rna_list->get_first()->get_obj();
+  list<ae_rna*> rna_list = indiv1->get_rna_list();
+  EXPECT_EQ(2, rna_list.size() );
+  ae_rna* rna = rna_list.front();
   EXPECT_EQ(LEADING, rna->get_strand());
   EXPECT_EQ(4, rna->get_promoter_pos());
   EXPECT_FLOAT_EQ(0.6, rna->get_basal_level());
   EXPECT_EQ(50, rna->get_transcript_length());
-  rna = rna_list->get_last()->get_obj();
+  rna = rna_list.back();
   EXPECT_EQ(LEADING, rna->get_strand());
   EXPECT_EQ(81, rna->get_promoter_pos());
   EXPECT_FLOAT_EQ(0.8, rna->get_basal_level());
   EXPECT_EQ(82, rna->get_transcript_length());
 
   // Check protein list
-  ae_list<ae_protein*>* prot_list = indiv1->get_protein_list();
-  EXPECT_EQ(1, prot_list->get_nb_elts());
-  ae_protein* prot = prot_list->get_first()->get_obj();
+  list<ae_protein*> prot_list = indiv1->get_protein_list();
+  EXPECT_EQ(1, prot_list.size());
+  ae_protein* prot = prot_list.front();
   EXPECT_EQ(LEADING, prot->get_strand());
   EXPECT_EQ(31, prot->get_shine_dal_pos());
   EXPECT_EQ(4, prot->get_length());
   EXPECT_FLOAT_EQ(1.4, prot->get_concentration());
-  EXPECT_EQ(2, prot->get_rna_list()->get_nb_elts());
+  EXPECT_EQ(2, prot->get_rna_list_std().size());
 }
 
 TEST_F(individualTest, TestIndiv2)
@@ -244,28 +247,28 @@ TEST_F(individualTest, TestIndiv2)
   EXPECT_EQ(109, indiv2->get_genetic_unit_seq_length(0));
 
   // Check RNA list
-  ae_list<ae_rna*>* rna_list = indiv2->get_rna_list();
-  EXPECT_EQ(2, rna_list->get_nb_elts());
-  ae_rna* rna = rna_list->get_first()->get_obj();
+  list<ae_rna*> rna_list = indiv2->get_rna_list();
+  EXPECT_EQ(2, rna_list.size());
+  ae_rna* rna = rna_list.front();
   EXPECT_EQ(LAGGING, rna->get_strand());
   EXPECT_EQ(104, rna->get_promoter_pos());
   EXPECT_FLOAT_EQ(0.6, rna->get_basal_level());
   EXPECT_EQ(50, rna->get_transcript_length());
-  rna = rna_list->get_last()->get_obj();
+  rna = rna_list.back();
   EXPECT_EQ(LAGGING, rna->get_strand());
   EXPECT_EQ(27, rna->get_promoter_pos());
   EXPECT_FLOAT_EQ(0.8, rna->get_basal_level());
   EXPECT_EQ(82, rna->get_transcript_length());
 
   // Check protein list
-  ae_list<ae_protein*>* prot_list = indiv2->get_protein_list();
-  EXPECT_EQ(1, prot_list->get_nb_elts());
-  ae_protein* prot = prot_list->get_first()->get_obj();
+  list<ae_protein*> prot_list = indiv2->get_protein_list();
+  EXPECT_EQ(1, prot_list.size());
+  ae_protein* prot = prot_list.front();
   EXPECT_EQ(LAGGING, prot->get_strand());
   EXPECT_EQ(77, prot->get_shine_dal_pos());
   EXPECT_EQ(4, prot->get_length());
   EXPECT_FLOAT_EQ(1.4, prot->get_concentration());
-  EXPECT_EQ(2, prot->get_rna_list()->get_nb_elts());
+  EXPECT_EQ(2, prot->get_rna_list_std().size());
 }
 
 TEST_F(individualTest, TestIndiv3)
@@ -275,23 +278,23 @@ TEST_F(individualTest, TestIndiv3)
   EXPECT_EQ(81, indiv3->get_genetic_unit_seq_length(0));
 
   // Check RNA list
-  ae_list<ae_rna*>* rna_list = indiv3->get_rna_list();
-  EXPECT_EQ(1, rna_list->get_nb_elts());
-  ae_rna* rna = rna_list->get_first()->get_obj();
+  list<ae_rna*> rna_list = indiv3->get_rna_list();
+  EXPECT_EQ(1, rna_list.size());
+  ae_rna* rna = rna_list.front();
   EXPECT_EQ(LEADING, rna->get_strand());
   EXPECT_EQ(54, rna->get_promoter_pos());
   EXPECT_FLOAT_EQ(0.8, rna->get_basal_level());
   EXPECT_EQ(42, rna->get_transcript_length());
 
   // Check protein list
-  ae_list<ae_protein*>* prot_list = indiv3->get_protein_list();
-  EXPECT_EQ(1, prot_list->get_nb_elts());
-  ae_protein* prot = prot_list->get_first()->get_obj();
+  list<ae_protein*> prot_list = indiv3->get_protein_list();
+  EXPECT_EQ(1, prot_list.size());
+  ae_protein* prot = prot_list.front();
   EXPECT_EQ(LEADING, prot->get_strand());
   EXPECT_EQ(4, prot->get_shine_dal_pos());
   EXPECT_EQ(4, prot->get_length());
   EXPECT_FLOAT_EQ(0.8, prot->get_concentration());
-  EXPECT_EQ(1, prot->get_rna_list()->get_nb_elts());
+  EXPECT_EQ(1, prot->get_rna_list_std().size());
 }
 
 TEST_F(individualTest, TestIndiv4)
@@ -301,23 +304,23 @@ TEST_F(individualTest, TestIndiv4)
   EXPECT_EQ(81, indiv4->get_genetic_unit_seq_length(0));
 
   // Check RNA list
-  ae_list<ae_rna*>* rna_list = indiv4->get_rna_list();
-  EXPECT_EQ(1, rna_list->get_nb_elts());
-  ae_rna* rna = rna_list->get_first()->get_obj();
+  list<ae_rna*> rna_list = indiv4->get_rna_list();
+  EXPECT_EQ(1, rna_list.size());
+  ae_rna* rna = rna_list.front();
   EXPECT_EQ(LAGGING, rna->get_strand());
   EXPECT_EQ(26, rna->get_promoter_pos());
   EXPECT_FLOAT_EQ(0.8, rna->get_basal_level());
   EXPECT_EQ(42, rna->get_transcript_length());
 
   // Check protein list
-  ae_list<ae_protein*>* prot_list = indiv4->get_protein_list();
-  EXPECT_EQ(1, prot_list->get_nb_elts());
-  ae_protein* prot = prot_list->get_first()->get_obj();
+  list<ae_protein*> prot_list = indiv4->get_protein_list();
+  EXPECT_EQ(1, prot_list.size());
+  ae_protein* prot = prot_list.front();
   EXPECT_EQ(LAGGING, prot->get_strand());
   EXPECT_EQ(76, prot->get_shine_dal_pos());
   EXPECT_EQ(4, prot->get_length());
   EXPECT_FLOAT_EQ(0.8, prot->get_concentration());
-  EXPECT_EQ(1, prot->get_rna_list()->get_nb_elts());
+  EXPECT_EQ(1, prot->get_rna_list_std().size());
 }
 
 // ===========================================================================
