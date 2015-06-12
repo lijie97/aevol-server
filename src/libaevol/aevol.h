20fb1cb3dd2a292af962871d88099b25c5a9df8b
David Parsons <david.parsons@inria.fr>
2015-06-12 10:47:35 +0200

    Add convenience header aevol.h
    aevol.h includes all the headers in aevol

---
+++ src/libaevol/aevol.h
@@ -0,0 +1,81 @@
+// ****************************************************************************
+//
+//          Aevol - An in silico experimental evolution platform
+//
+// ****************************************************************************
+//
+// Copyright: See the AUTHORS file provided with the package or <www.aevol.fr>
+// Web: http://www.aevol.fr/
+// E-mail: See <http://www.aevol.fr/contact/>
+// Original Authors : Guillaume Beslon, Carole Knibbe, David Parsons
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ****************************************************************************
+
+
+#ifndef AEVOL_INCLUDE_AEVOL_H__
+#define AEVOL_INCLUDE_AEVOL_H__
+
+#include "ae_enums.h"
+#include "ae_string.h"
+#include "ae_utils.h"
+#include "Alignment.h"
+#include "Codon.h"
+#include "Dna.h"
+#include "DnaReplicationReport.h"
+#include "Dump.h"
+#include "ExpManager.h"
+#include "ExpManager_X11.h"
+#include "ExpSetup.h"
+#include "f_line.h"
+#include "Fuzzy.h"
+#include "Gaussian.h"
+#include "GeneMutation.h"
+#include "GeneticUnit.h"
+#include "GeneTree.h"
+#include "GeneTreeNode.h"
+#include "GridCell.h"
+#include "habitat.h"
+#include "IndividualFactory.h"
+#include "Individual.h"
+#include "Individual_X11.h"
+#include "indiv_stats.h"
+#include "JumpingMT.h"
+#include "JumpPoly.h"
+#include "Logging.h"
+#include "macros.h"
+#include "Mutation.h"
+#include "MutationParams.h"
+#include "non_coding_stats.h"
+#include "OutputManager.h"
+#include "param_loader.h"
+#include "phenotype.h"
+#include "PhenotypicSegment.h"
+#include "phenotypic_target.h"
+#include "phenotypic_target_handler.h"
+#include "point.h"
+#include "Protein.h"
+#include "ReplicationReport.h"
+#include "Rna.h"
+#include "Selection.h"
+#include "StatRecord.h"
+#include "Stats.h"
+#include "Time.h"
+#include "Tree.h"
+#include "VisAVis.h"
+#include "world.h"
+#include "X11Window.h"
+
+#endif  // AEVOL_INCLUDE_AEVOL_H__
