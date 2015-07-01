449c4ba71c9a26c1ae78c43bf6438c38f5dd7e50
Vincent Liard <vincent.liard@inria.fr>
2015-07-01 13:33:37 +0200

    don't add observers to trees when there is no tree

--- src/libaevol/ExpManager.cpp
+++ src/libaevol/ExpManager.cpp
@@ -275,13 +275,14 @@ void ExpManager::load(gzFile& exp_s_file,
   printf(" OK\n");
 
   // -------------------------------------------- Link world and output profile
-  get_sel()->addObserver(get_tree(), NEW_INDIV);
-  get_sel()->addObserver(get_tree(), END_GENERATION);
-  if (get_record_tree())
+  if (get_record_tree()) {
+    get_sel()->addObserver(get_tree(), NEW_INDIV);
+    get_sel()->addObserver(get_tree(), END_GENERATION);
     for (auto indiv : world_->get_indivs())
       indiv->addObserver(
           get_tree()->get_report_by_index(Time::get_time(), indiv->get_id()),
           END_REPLICATION);
+  }
 }
 
 
