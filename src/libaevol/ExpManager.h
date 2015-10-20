30519769b4281f51e22b8000b76ac3a5dad1c282
Vincent Liard <vincent.liard@inria.fr>
2015-10-20 17:32:47 +0200

    fix ExpManager's indentation

--- src/libaevol/ExpManager.h
+++ src/libaevol/ExpManager.h
@@ -27,8 +27,8 @@
 // =================================================================
 //                              Includes
 // =================================================================
-#ifndef AEVOL_EXP_MANAGER_H__
-#define AEVOL_EXP_MANAGER_H__
+#ifndef AEVOL_EXP_MANAGER_H_
+#define AEVOL_EXP_MANAGER_H_
 
 #include <cinttypes>
 #include <cstdio>
@@ -44,10 +44,7 @@
 #include "Observer.h"
 #include "ObservableEvent.h"
 
-
 namespace aevol {
-
-
 // =================================================================
 //                          Class declarations
 // =================================================================
@@ -59,148 +56,148 @@ namespace aevol {
 /// pair of aevol binary files (pop and exp_setup)
 
 class ExpManager : public Observer {
-  public :
-    // =======================================================================
-    //                                Constructors
-    // =======================================================================
-    ExpManager();
+ public:
+  // =======================================================================
+  //                                Constructors
+  // =======================================================================
+  ExpManager();
 
-    // =======================================================================
-    //                                Destructors
-    // =======================================================================
-    virtual ~ExpManager();
+  // =======================================================================
+  //                                Destructors
+  // =======================================================================
+  virtual ~ExpManager();
 
-    // =======================================================================
-    //                                 Algorithms
-    // =======================================================================
+  // =======================================================================
+  //                                 Algorithms
+  // =======================================================================
 
-    // =======================================================================
-    //                           Accessors: getters
-    // =======================================================================
-    ExpSetup* get_exp_s(void) const { return _exp_s; }
-    Selection* get_sel(void) const { return get_exp_s()->get_sel(); }
-    OutputManager* get_output_m(void) const { return _output_m; }
-    bool quit_signal_received(void) const { return _quit_signal_received; }
-    SelectionScheme get_selection_scheme(void) const { return get_sel()->get_selection_scheme(); }
-    double get_selection_pressure(void) const { return get_sel()->get_selection_pressure(); }
+  // =======================================================================
+  //                           Accessors: getters
+  // =======================================================================
+  ExpSetup* get_exp_s(void) const { return _exp_s; }
+  Selection* get_sel(void) const { return get_exp_s()->get_sel(); }
+  OutputManager* get_output_m(void) const { return _output_m; }
+  bool quit_signal_received(void) const { return _quit_signal_received; }
+  SelectionScheme get_selection_scheme(void) const { return get_sel()->get_selection_scheme(); }
+  double get_selection_pressure(void) const { return get_sel()->get_selection_pressure(); }
 
-    // Spatial structure
-    World* world(void) const { return world_; }
-    GridCell* get_grid_cell(int16_t x, int16_t y) const { return world()->grid(x, y); }
-    int16_t get_grid_width(void) const { return world()->width(); }
-    int16_t get_grid_height(void) const { return world()->height(); }
-    GridCell*** get_pop_grid(void) const { return world()->grid(); }
+  // Spatial structure
+  World* world(void) const { return world_; }
+  GridCell* get_grid_cell(int16_t x, int16_t y) const { return world()->grid(x, y); }
+  int16_t get_grid_width(void) const { return world()->width(); }
+  int16_t get_grid_height(void) const { return world()->height(); }
+  GridCell*** get_pop_grid(void) const { return world()->grid(); }
 
-    // Global settings
-    double get_repl_HT_detach_rate(void) const { return get_exp_s()->get_repl_HT_detach_rate();}
+  // Global settings
+  double get_repl_HT_detach_rate(void) const { return get_exp_s()->get_repl_HT_detach_rate();}
 
-    // The ability to own a plasmid is a property of the individuals (allow_plasmids_) because it is used during mutations
-    // However there is also a property of the experimental setup (_with_plasmids) that indicates whether plasmids are used because we need this during replication and during loading/writting
-    // For now when plasmids are used each individual has one and only one plasmid (so these variables should always be equals), however this may change in the future
-    // There is no longer property _with_plasmids_HT because the ability to transfer is evolvable and thus may depend on the plasmid itself
+  // The ability to own a plasmid is a property of the individuals (allow_plasmids_) because it is used during mutations
+  // However there is also a property of the experimental setup (_with_plasmids) that indicates whether plasmids are used because we need this during replication and during loading/writting
+  // For now when plasmids are used each individual has one and only one plasmid (so these variables should always be equals), however this may change in the future
+  // There is no longer property _with_plasmids_HT because the ability to transfer is evolvable and thus may depend on the plasmid itself
 
-    bool   get_with_plasmids(void) const { return get_exp_s()->get_with_plasmids(); }
-    double get_prob_plasmid_HT(void) const { return get_exp_s()->get_prob_plasmid_HT(); }
-    double get_tune_donor_ability(void) const { return get_exp_s()->get_tune_donor_ability(); }
-    double get_tune_recipient_ability(void) const { return get_exp_s()->get_tune_recipient_ability(); }
-    bool   get_swap_GUs(void) const { return get_exp_s()->get_swap_GUs(); }
-    bool   get_with_secretion(void) const { return get_exp_s()->get_with_secretion(); }
-    double get_secretion_contrib_to_fitness(void) const { return get_exp_s()->get_secretion_contrib_to_fitness(); }
-    double get_secretion_cost(void) const { return get_exp_s()->get_secretion_cost(); }
+  bool   get_with_plasmids(void) const { return get_exp_s()->get_with_plasmids(); }
+  double get_prob_plasmid_HT(void) const { return get_exp_s()->get_prob_plasmid_HT(); }
+  double get_tune_donor_ability(void) const { return get_exp_s()->get_tune_donor_ability(); }
+  double get_tune_recipient_ability(void) const { return get_exp_s()->get_tune_recipient_ability(); }
+  bool   get_swap_GUs(void) const { return get_exp_s()->get_swap_GUs(); }
+  bool   get_with_secretion(void) const { return get_exp_s()->get_with_secretion(); }
+  double get_secretion_contrib_to_fitness(void) const { return get_exp_s()->get_secretion_contrib_to_fitness(); }
+  double get_secretion_cost(void) const { return get_exp_s()->get_secretion_cost(); }
 
-    // Accessors to population stuff
-    std::list<Individual*> get_indivs() const { return world()->get_indivs(); }
-    int32_t get_nb_indivs(void) const { return world()->get_nb_indivs(); }
-    Individual* get_best_indiv(void) const { return world()->get_best_indiv(); }
-    Individual* get_indiv_by_id(int32_t id) const;
+  // Accessors to population stuff
+  std::list<Individual*> get_indivs() const { return world()->get_indivs(); }
+  int32_t get_nb_indivs(void) const { return world()->get_nb_indivs(); }
+  Individual* get_best_indiv(void) const { return world()->get_best_indiv(); }
+  Individual* get_indiv_by_id(int32_t id) const;
 
-    // Accessors to output manager stuff
-    int64_t	get_backup_step(void) const { return get_output_m()->get_backup_step(); }
-    bool get_record_tree(void) const { return get_output_m()->get_record_tree(); }
-    int32_t get_tree_step(void) const { return get_output_m()->get_tree_step(); }
-    Tree* get_tree(void) const { return get_output_m()->get_tree(); }
+  // Accessors to output manager stuff
+  int64_t	get_backup_step(void) const { return get_output_m()->get_backup_step(); }
+  bool get_record_tree(void) const { return get_output_m()->get_record_tree(); }
+  int32_t get_tree_step(void) const { return get_output_m()->get_tree_step(); }
+  Tree* get_tree(void) const { return get_output_m()->get_tree(); }
 
-    // =======================================================================
-    //                          Accessors: setters
-    // =======================================================================
-    void set_t_end(int64_t _t_end) { t_end = _t_end; }
-    void set_HT_ins_rate(double HT_ins_rate) { _exp_s->set_HT_ins_rate(HT_ins_rate); }
-    void set_HT_repl_rate(double HT_repl_rate) { _exp_s->set_HT_repl_rate(HT_repl_rate); }
+  // =======================================================================
+  //                          Accessors: setters
+  // =======================================================================
+  void set_t_end(int64_t _t_end) { t_end = _t_end; }
+  void set_HT_ins_rate(double HT_ins_rate) { _exp_s->set_HT_ins_rate(HT_ins_rate); }
+  void set_HT_repl_rate(double HT_repl_rate) { _exp_s->set_HT_repl_rate(HT_repl_rate); }
 
-    // =======================================================================
-    //                                 Operators
-    // =======================================================================
+  // =======================================================================
+  //                                 Operators
+  // =======================================================================
 
-    // =======================================================================
-    //                               Public Methods
-    // =======================================================================
-    void InitializeWorld(int16_t grid_width,
-                         int16_t grid_height,
-                         std::shared_ptr<JumpingMT> prng,
-                         const Habitat& habitat,
-                         bool share_phenotypic_target);
-    void Save() const;
-    void WriteSetupFiles() const;
-    void WriteDynamicFiles() const;
-    void save_copy(char* dir, int64_t time = 0) const;
-    void load(int64_t first_gener, bool verbose = false, bool to_be_run = true) { load(".", first_gener, verbose, to_be_run); }
-    void load(const char* dir, int64_t t0,
-        bool verbose = false, bool to_be_run = true);
-    /// Load an experiment with default files from the current directory
-    void load(int64_t t0, char* exp_s_file, char* exp_backup_file, char* sp_struct_file, char* out_p_file,
-              bool verbose = false, bool to_be_run = true);
-    void run_evolution(void);
-    virtual void display(void) {};
-    void update_best(void);
-    void FillGridWithClones(Individual& dolly) { world_->FillGridWithClones(dolly); }
-    void update(Observable& o, ObservableEvent e, void* arg) override {}
+  // =======================================================================
+  //                               Public Methods
+  // =======================================================================
+  void InitializeWorld(int16_t grid_width,
+                       int16_t grid_height,
+                       std::shared_ptr<JumpingMT> prng,
+                       const Habitat& habitat,
+                       bool share_phenotypic_target);
+  void Save() const;
+  void WriteSetupFiles() const;
+  void WriteDynamicFiles() const;
+  void save_copy(char* dir, int64_t time = 0) const;
+  void load(int64_t first_gener, bool verbose = false, bool to_be_run = true) { load(".", first_gener, verbose, to_be_run); }
+  void load(const char* dir, int64_t t0,
+            bool verbose = false, bool to_be_run = true);
+  /// Load an experiment with default files from the current directory
+  void load(int64_t t0, char* exp_s_file, char* exp_backup_file, char* sp_struct_file, char* out_p_file,
+            bool verbose = false, bool to_be_run = true);
+  void run_evolution(void);
+  virtual void display(void) {};
+  void update_best(void);
+  void FillGridWithClones(Individual& dolly) { world_->FillGridWithClones(dolly); }
+  void update(Observable& o, ObservableEvent e, void* arg) override {}
 
-  protected :
-    // =======================================================================
-    //                              Protected Methods
-    // =======================================================================
-    void step_to_next_generation();
+ protected:
+  // =======================================================================
+  //                              Protected Methods
+  // =======================================================================
+  void step_to_next_generation();
 
-    void load(gzFile& exp_s_file,
-              gzFile& exp_backup_file,
-              gzFile& sp_struct_file,
-              gzFile& out_p_file,
-              bool verbose = false,
-              bool to_be_run = true);
+  void load(gzFile& exp_s_file,
+            gzFile& exp_backup_file,
+            gzFile& sp_struct_file,
+            gzFile& out_p_file,
+            bool verbose = false,
+            bool to_be_run = true);
 
-    void create_missing_directories(const char* dir = ".") const;
-    void open_backup_files(gzFile& sel_file,
-                           gzFile& sp_struct_file,
-                           int64_t t,
-                           const char mode[3],
-                           const char* dir = ".") const;
-    void close_backup_files(gzFile& sel_file,
-                            gzFile& sp_struct_file) const;
-    void open_setup_files(gzFile& exp_s_gzfile,
-                          gzFile& out_p_gzfile,
-                          int64_t t,
-                          const char mode[3],
-                          const char* dir = ".") const;
-    void close_setup_files(gzFile& exp_s_gzfile, gzFile& out_p_gzfile) const;
+  void create_missing_directories(const char* dir = ".") const;
+  void open_backup_files(gzFile& sel_file,
+                         gzFile& sp_struct_file,
+                         int64_t t,
+                         const char mode[3],
+                         const char* dir = ".") const;
+  void close_backup_files(gzFile& sel_file,
+                          gzFile& sp_struct_file) const;
+  void open_setup_files(gzFile& exp_s_gzfile,
+                        gzFile& out_p_gzfile,
+                        int64_t t,
+                        const char mode[3],
+                        const char* dir = ".") const;
+  void close_setup_files(gzFile& exp_s_gzfile, gzFile& out_p_gzfile) const;
 
-    // =======================================================================
-    //                             Protected Attributes
-    // =======================================================================
-    /// Experimental setup
-    ExpSetup* _exp_s;
+  // =======================================================================
+  //                             Protected Attributes
+  // =======================================================================
+  /// Experimental setup
+  ExpSetup* _exp_s;
 
-    /// Spatial structure
-    World* world_;
+  /// Spatial structure
+  World* world_;
 
-    /// Output manager
-    OutputManager* _output_m;
+  /// Output manager
+  OutputManager* _output_m;
 
-    /// Time step up to which we want to simulate
-    int64_t t_end;
+  /// Time step up to which we want to simulate
+  int64_t t_end;
 
-    // Set to true when ctrl-Q is received. Will cause the simulation
-    // to be ended after the current time step is completed
-    bool _quit_signal_received;
+  // Set to true when ctrl-Q is received. Will cause the simulation
+  // to be ended after the current time step is completed
+  bool _quit_signal_received;
 };
 
 // ===========================================================================
@@ -220,4 +217,4 @@ class ExpManager : public Observer {
 // ===========================================================================
 
 } // namespace aevol
-#endif // AEVOL_EXP_MANAGER_H__
+#endif // AEVOL_EXP_MANAGER_H_
