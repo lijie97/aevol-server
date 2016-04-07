824e8d7193f765d65af8753151ace64246f1f376
David Parsons <david.parsons@inria.fr>
2016-04-07 17:10:04 +0200

    Remove code duplication in Stats
    In method write_current_generation_statistics

--- src/libaevol/StatRecord.cpp
+++ src/libaevol/StatRecord.cpp
@@ -679,174 +679,100 @@ void StatRecord::initialize_data()
 
 void StatRecord::write_to_file(FILE* stat_file, stats_type stat_type_to_print) const
 {
-  if (record_type_ == INDIV)
+  if (stat_type_to_print == FITNESS_STATS)
   {
-    if (stat_type_to_print == FITNESS_STATS)
-    {
+    fprintf(stat_file,
+        "%" PRId64 " %" PRId32 " %e %" PRId32 " %e %e %e %e %e %e %e",
+        AeTime::time(),
+        pop_size_,
+        fitness_,
+        amount_of_dna_,
+        metabolic_error_,
+        parent_metabolic_error_,
+        metabolic_fitness_,
+        secretion_error_,
+        parent_secretion_error_,
+        secretion_fitness_,
+        compound_amount_);
+    #ifdef __REGUL
       fprintf(stat_file,
-          "%" PRId64 " %" PRId32 " %e %" PRId32 " %e %e %e %e %e %e %e",
-          AeTime::time(),
-          pop_size_,
-          fitness_,
-          amount_of_dna_,
-          metabolic_error_,
-          parent_metabolic_error_,
-          metabolic_fitness_,
-          secretion_error_,
-          parent_secretion_error_,
-          secretion_fitness_,
-          compound_amount_);
-      #ifdef __REGUL
-        fprintf(stat_file,
-            " %" PRId32 " %" PRId32 " %" PRId32 " %f %f %f",
-            nb_influences_,
-            nb_enhancing_influences_,
-            nb_operating_influences_,
-            av_value_influences_,
-            av_value_enhancing_influences_,
-            av_value_operating_influences_);
-      #endif
+          " %" PRId32 " %" PRId32 " %" PRId32 " %f %f %f",
+          nb_influences_,
+          nb_enhancing_influences_,
+          nb_operating_influences_,
+          av_value_influences_,
+          av_value_enhancing_influences_,
+          av_value_operating_influences_);
+    #endif
+  }
+  if (stat_type_to_print == MUTATION_STATS)
+  {
+    fprintf(stat_file,
+        "%" PRId64 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 "",
+        AeTime::time(),
+        nb_mut_,
+        nb_rear_,
+        nb_switch_,
+        nb_indels_,
+        nb_dupl_,
+        nb_del_,
+        nb_trans_,
+        nb_inv_);
+  }
+  if (stat_type_to_print == GENES_STATS)
+  {
+    fprintf(stat_file,
+        "%" PRId64 " %" PRId32 " %" PRId32 " %f %f %" PRId32 " %" PRId32 " %f %f ",
+        AeTime::time(),
+        nb_coding_rnas_,
+        nb_non_coding_rnas_,
+        av_size_coding_rnas_,
+        av_size_non_coding_rnas_,
+        nb_functional_genes_,
+        nb_non_functional_genes_,
+        av_size_functional_gene_,
+        av_size_non_functional_gene_);
+  }
+  if (stat_type_to_print == BP_STATS) {
+    if (record_type_ == INDIV) {
+      fprintf(stat_file,
+              "%" PRId64 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 "",
+              AeTime::time(),
+              nb_bases_in_0_CDS_,
+              nb_bases_in_0_functional_CDS_,
+              nb_bases_in_0_non_functional_CDS_,
+              nb_bases_in_0_RNA_,
+              nb_bases_in_0_coding_RNA_,
+              nb_bases_in_0_non_coding_RNA_,
+              nb_bases_non_essential_,
+              nb_bases_non_essential_including_nf_genes_);
     }
-    if (stat_type_to_print == MUTATION_STATS)
+    else // if record_type_ == POP
     {
-      fprintf(stat_file,
-          "%" PRId64 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 "",
-          AeTime::time(),
-          nb_mut_,
-          nb_rear_,
-          nb_switch_,
-          nb_indels_,
-          nb_dupl_,
-          nb_del_,
-          nb_trans_,
-          nb_inv_);
-    }
-    if (stat_type_to_print == GENES_STATS)
-    {
-      fprintf(stat_file,
-          "%" PRId64 " %" PRId32 " %" PRId32 " %f %f %" PRId32 " %" PRId32 " %f %f ",
-          AeTime::time(),
-          nb_coding_rnas_,
-          nb_non_coding_rnas_,
-          av_size_coding_rnas_,
-          av_size_non_coding_rnas_,
-          nb_functional_genes_,
-          nb_non_functional_genes_,
-          av_size_functional_gene_,
-          av_size_non_functional_gene_);
-    }
-    if (stat_type_to_print == BP_STATS)
-    {
-      fprintf(stat_file,
-          "%" PRId64 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 "",
-          AeTime::time(),
-          nb_bases_in_0_CDS_,
-          nb_bases_in_0_functional_CDS_,
-          nb_bases_in_0_non_functional_CDS_,
-          nb_bases_in_0_RNA_,
-          nb_bases_in_0_coding_RNA_,
-          nb_bases_in_0_non_coding_RNA_,
-          nb_bases_non_essential_,
-          nb_bases_non_essential_including_nf_genes_);
-    }
-    if (stat_type_to_print == REAR_STATS)
-    {
-      fprintf(stat_file,
-          "%" PRId64 " %e %e %e %e %f",
-          AeTime::time(),
-          dupl_rate_,
-          del_rate_,
-          trans_rate_,
-          inv_rate_,
-          mean_align_score_);
+      // TO DO (if needed) : base-pair stats for all individuals, not just for the best one.
+      //
+      // fprintf(stat_file, "%" PRId64 " %f %f %f %f %f %f %f %f",
+      //         AeTime::time(),
+      //         nb_bases_in_0_CDS_,
+      //         nb_bases_in_0_functional_CDS_,
+      //         nb_bases_in_0_non_functional_CDS_,
+      //         nb_bases_in_0_RNA_,
+      //         nb_bases_in_0_coding_RNA_,
+      //         nb_bases_in_0_non_coding_RNA_,
+      //         nb_bases_non_essential_,
+      //         nb_bases_non_essential_including_nf_genes_);
     }
   }
-  else // if record_type_ == POP
+  if (stat_type_to_print == REAR_STATS)
   {
-   if (stat_type_to_print == FITNESS_STATS)
-    {
-      fprintf(stat_file,
-          "%" PRId64 " %" PRId32 " %e %" PRId32 " %e %e %e %e %e %e %e",
-          AeTime::time(),
-          pop_size_,
-          fitness_,
-          amount_of_dna_,
-          metabolic_error_,
-          parent_metabolic_error_,
-          metabolic_fitness_,
-          secretion_error_,
-          parent_secretion_error_,
-          secretion_fitness_,
-          compound_amount_);
-
-      #ifdef __REGUL
-        fprintf(stat_file,
-            " %" PRId32 " %" PRId32 " %" PRId32 " %f %f %f",
-            nb_influences_,
-            nb_enhancing_influences_,
-            nb_operating_influences_,
-            av_value_influences_,
-            av_value_enhancing_influences_,
-            av_value_operating_influences_);
-      #endif
-    }
-    if (stat_type_to_print == MUTATION_STATS)
-    {
-      fprintf(stat_file,
-          "%" PRId64 " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32
-          " %" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 "",
-          AeTime::time(),
-          nb_mut_,
-          nb_rear_,
-          nb_switch_,
-          nb_indels_,
-          nb_dupl_,
-          nb_del_,
-          nb_trans_,
-          nb_inv_);
-
-    }
-    if (stat_type_to_print == GENES_STATS)
-    {
-      fprintf(stat_file,
-          "%" PRId64 " %" PRId32 " %" PRId32 " %f %f %" PRId32 " %" PRId32
-          " %f %f",
-          AeTime::time(),
-          nb_coding_rnas_,
-          nb_non_coding_rnas_,
-          av_size_coding_rnas_,
-          av_size_non_coding_rnas_,
-          nb_functional_genes_,
-          nb_non_functional_genes_,
-          av_size_functional_gene_,
-          av_size_non_functional_gene_);
-    }
-    if (stat_type_to_print == BP_STATS)
-    {
-     // TO DO (if needed) : base-pair stats for all individuals, not just for the best one.
-     //
-     // fprintf(stat_file, "%" PRId64 " %f %f %f %f %f %f %f %f",
-     //         AeTime::time(),
-     //         nb_bases_in_0_CDS_,
-     //         nb_bases_in_0_functional_CDS_,
-     //         nb_bases_in_0_non_functional_CDS_,
-     //         nb_bases_in_0_RNA_,
-     //         nb_bases_in_0_coding_RNA_,
-     //         nb_bases_in_0_non_coding_RNA_,
-     //         nb_bases_non_essential_,
-     //         nb_bases_non_essential_including_nf_genes_);
-    }
-    if (stat_type_to_print == REAR_STATS)
-    {
-      fprintf(stat_file,
-          "%" PRId64 " %e %e %e %e %f",
-          AeTime::time(),
-          dupl_rate_,
-          del_rate_,
-          trans_rate_,
-          inv_rate_,
-          mean_align_score_);
-    }
+    fprintf(stat_file,
+        "%" PRId64 " %e %e %e %e %f",
+        AeTime::time(),
+        dupl_rate_,
+        del_rate_,
+        trans_rate_,
+        inv_rate_,
+        mean_align_score_);
   }
 
   fprintf(stat_file, "\n");
