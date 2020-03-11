f4a72d59f47dbf6d091e4936889fab3916952296
Théotime Grohens <theotime.grohens@insa-lyon.fr>
2020-03-11 16:30:09 +0100

    Remove Raevol dependency in HabitatFactory files
    This commit removes dependencies on Raevol headers and classes in
    HabitatFactory.{h,cpp}, when compiled in regular Aevol mode.
    
    This enforces better code separation and removes some warnings that
    came from (quite possibly dead) Raevol code.

--- src/libaevol/world/HabitatFactory.h
+++ src/libaevol/world/HabitatFactory.h
@@ -5,29 +5,31 @@
 #ifndef AEVOL_REGUL_HABITATFACTORY_H
 #define AEVOL_REGUL_HABITATFACTORY_H
 
+#ifndef __REGUL
 #include "Habitat.h"
+#else
 #include "raevol/Habitat_R.h"
+#endif
+
 #if __cplusplus == 201103L
 #include "make_unique.h"
 #endif
 
 namespace aevol {
 
-
 class HabitatFactory {
  public:
+#ifndef __REGUL
   static std::unique_ptr<Habitat> create_unique_habitat(Habitat& habitat,
                                                         bool share_phenotypic_target);
+  static std::unique_ptr<Habitat>create_unique_habitat(gzFile backup_file,
+                            PhenotypicTargetHandler* phenotypic_target_handler);
+#else
   static std::unique_ptr<Habitat_R> create_unique_habitat(Habitat_R& habitat,
 														  bool share_phenotypic_target);
-  static
-#ifdef __REGUL
-    std::unique_ptr<Habitat_R>
-#else
-    std::unique_ptr<Habitat>
-#endif
-    create_unique_habitat(gzFile backup_file,
+  static std::unique_ptr<Habitat_R> create_unique_habitat(gzFile backup_file,
                  					  PhenotypicTargetHandler* phenotypic_target_handler);
+#endif
 
 };
 
