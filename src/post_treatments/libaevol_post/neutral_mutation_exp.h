e4dbcabdbe9a3041b6f5d097b54884397e8184f5
Marco Foley <marco.foley@inria.fr>
2020-09-28 14:23:29 +0200

    import neutral mutations related post treatement

---
+++ src/post_treatments/libaevol_post/neutral_mutation_exp.h
@@ -0,0 +1,27 @@
+//
+// Created by duazel on 11/03/2020.
+//
+
+#ifndef AEVOL_NEUTRAL_MUTATION_EXP_H
+#define AEVOL_NEUTRAL_MUTATION_EXP_H
+
+#include <string>
+
+#include "ExpManager.h"
+#include "Individual.h"
+#include "MutationParams.h"
+
+using namespace aevol;
+
+Individual *new_child(Individual *individual,
+                      std::vector<MutationEvent *> &mutations,
+                      std::vector<int32_t> &lengths);
+Individual *new_child(Individual *individual, std::vector<MutationEvent *> &mutations,
+                      std::vector<bool> &is_neutral);
+
+Individual *neutral_mutation(Individual *individual);
+
+Individual * run_generations(unsigned int nb_generations, const Individual & indiv);
+Individual * run_to_size(int32_t wanted_size, const Individual & indiv);
+
+#endif //AEVOL_NEUTRAL_MUTATION_EXP_H
