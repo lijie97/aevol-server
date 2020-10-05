5af1b5f526dc965a1125e32d307d958d6790cbd3
ROUZAUD-CORNABAS Jonathan <jonathan.rouzaud-cornabas@inria.fr>
2020-10-05 12:07:31 +0200

    [Selection] Change the order of execution of compute statistics to avoid segfault when running with OpenMP. Nevertheless, there is something wrong with the management of dynamically allocated memory and OpenMP within Aevol 6 version of the code

--- src/libaevol/population/Selection.cpp
+++ src/libaevol/population/Selection.cpp
@@ -363,6 +363,7 @@ void Selection::step_to_next_generation() {
           //                    world->indiv_at(x, y), x, y);
           // Tell observers the replication is finished
           //->notifyObservers(END_REPLICATION, eindiv);
+          world->indiv_at(x, y)->compute_statistical_data();
           world->indiv_at(x, y)->compute_non_coding();
           //delete eindiv;
         }
@@ -963,7 +964,6 @@ if (exp_m_->record_tree() || exp_m_->light_tree()) {
 
   return new_indiv;
 }
-
 
 #ifdef __REGUL
 void Selection::run_life(Individual_R* new_indiv) {
